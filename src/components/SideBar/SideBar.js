427b595c83478525836838a259d26637a74a626b
sammyalwayscode <olorundasamuel2@gmail.com>
2022-06-11 23:50:01 +0100

    The Dash

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
 
 const SideBar = () => {
+  const [tabChange, setTabChange] = useState(false);
+  const [tabDiaplay, setTabDisplay] = useState(true);
+
+  const tabChangeHandler = () => {
+    setTabChange(!tabChange);
+  };
+
+  const tabDiaplayHandler = () => {
+    setTabDisplay(!tabDiaplay);
+  };
+
   return (
-    <Container>
-      <Wrapper>SideBar</Wrapper>
-    </Container>
+    <>
+      {tabChange ? (
+        <TabContainer>
+          <TabWrapper>
+            <TabBar onClick={tabChangeHandler}>
+              <AiOutlineMenuUnfold />
+            </TabBar>
+          </TabWrapper>
+        </TabContainer>
+      ) : (
+        <Container>
+          <Wrapper>
+            <Bar onClick={tabChangeHandler}>
+              <AiOutlineMenuUnfold />
+            </Bar>
+          </Wrapper>
+        </Container>
+      )}
+
+      {tabDiaplay ? (
+        <TabDisplayContainer>
+          <TabDisplayWrapper>
+            <TabBar onClick={tabDiaplayHandler}>
+              <AiOutlineMenuUnfold />
+            </TabBar>
+          </TabDisplayWrapper>
+        </TabDisplayContainer>
+      ) : (
+        <ContainerDisplay>
+          <WrapperDiaplay>
+            <Bar onClick={tabDiaplayHandler}>
+              <AiOutlineMenuUnfold />
+            </Bar>
+          </WrapperDiaplay>
+        </ContainerDisplay>
+      )}
+    </>
   );
 };
 
@@ -16,5 +62,43 @@ const Container = styled.div`
   min-height: calc(100vh - 50px);
   background-color: darkred;
   width: 200px;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 const Wrapper = styled.div``;
+
+const TabContainer = styled.div`
+  margin-top: 50px;
+  min-height: calc(100vh - 50px);
+  background-color: darkcyan;
+  width: 50px;
+`;
+const TabWrapper = styled.div``;
+
+const TabBar = styled.div`
+  font-size: 25px;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const Bar = styled.div`
+  font-size: 25px;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const TabDisplayContainer = styled.div`
+  display: none;
+  @media (max-width: 770px) {
+    display: block;
+    margin-top: 50px;
+    min-height: calc(100vh - 50px);
+    background-color: darkcyan;
+    width: 50px;
+  }
+`;
+
+const TabDisplayWrapper = styled.div``;
+const ContainerDisplay = styled.div``;
+const WrapperDiaplay = styled.div``;
